1d67fb8018d53c6dd429af8bebac081e1e7c5131
afdk <afdk@192.168.56.1>
2016-08-28 15:39:39 +0700

    setting halaman HOME

--- application/views/home.php
+++ application/views/home.php
@@ -1,12 +1,43 @@
 <div class="row">
-        <div class="span12">
+        <div class="span6">
             <div class="widget-box">
                 <div class="widget-header">
                     <i class="icon-info-sign"></i>
-                    <h3>Hello World</h3>
+                    <h3>Selamat Datang</h3>
                 </div>
                 <div class="widget-content">
-                    <div class="alert alert-info">Say K people!</div>
+                    <p>Selamat datang di aplikasi Pro-KMS</p>
+                    <p>Aplikasi ini adalah aplikasi penunjang untuk melakukan pencatatan dan monitoring tumbuh kemabng bayi dan balita.
+                        Prinsip-prinsip dasar yang digunakan pada aplikasi ini adalah <b>Kartu Menuju Sehat (KMS)</b> yang dikeluarkan oleh 
+                        Kementrian Kesehatan Republik Indonesia tahun 2010.
+                    </p>
+                </div>
+            </div>
+        </div>
+    
+        <div class="span6">
+            <div class="widget-box">
+                <div class="widget-header">
+                    <i class="icon-info-sign"></i>
+                    <h3>Hal penting</h3>
+                </div>
+                <div class="widget-content">
+                    <p>Hal-hal penting yang perlu digunakan dalam menggunakan aplikasi ini:</p>
+                    <ol>
+                        <li>
+                            Hendaklah mengisi perkembangan anak balita anda setiap dilakukan pengukuran, minimal dalam 1 bulan sekali.
+                        </li>
+                        <li>
+                            Pastikan data jenis kelamin balita yang didaftarkan pada aplikasi sudah benar, karena akan mempengaruhi grafik perkembangan.
+                        </li>
+                        <li>
+                            Patikan data tanggal lahir balita yang didaftarkan pada aplikasi sudah benar.
+                        </li>
+                        <li>
+                            Hendaklah melakukan pengecekan data Agenda Imunisasi, agar tidak terjadi keterlamabatan imunisasi.
+                        </li>
+                    </ol>
+                    
                 </div>
             </div>
         </div>
